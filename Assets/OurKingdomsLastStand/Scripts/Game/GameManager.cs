9aad74eb6db1cb7704d33504465b25d823017693
abig006@icloud.com <abig006@icloud.com>
2025-12-03 11:35:00 +0000

    Added power ups

--- Assets/OurKingdomsLastStand/Scripts/Game/GameManager.cs
+++ Assets/OurKingdomsLastStand/Scripts/Game/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -10,7 +11,10 @@ public class GameManager : MonoBehaviour
     public bool paused;
     public bool gameOver = false;
     public bool fastForward = false;
+    public bool slowPowerUpActive = false;
+    public bool freezePowerUpActive = false;
     public int wavesRemaining = 10;
+    public float currentEnemySpeed;
     public float bossTimer = 150; //time the player has to kill the boss
     public TextMeshProUGUI coinText;
     public TextMeshProUGUI wavesText;
@@ -20,6 +24,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject GameOverScreen;
     [SerializeField] GameObject GameWinScreen;
     [SerializeField] GameObject Tutorial;
+    public Button slowPowerUp;
+    public Button freezePowerUp;
+    public Button speedUp;
 
     [Header("Stats")]
     public int wavesSurvived = -1; //starts at -1 so that when the first wave is spawned there are 0 waves survived
@@ -40,6 +47,36 @@ public class GameManager : MonoBehaviour
     {
         coinText.text = "Coins: " + coinsRemaining + "";
         wavesText.text = "Waves Remaining: " + wavesRemaining;
+        if (slowPowerUpActive == true)
+        {
+            return;
+        }
+        else
+        {
+            if (coinsRemaining < 500)
+            {
+                slowPowerUp.interactable = false;
+            }
+            else if (coinsRemaining >= 500)
+            {
+                slowPowerUp.interactable = true;
+            }
+        }
+        if (freezePowerUpActive == true)
+        {
+            return;
+        }
+        else
+        {
+            if (coinsRemaining < 750)
+            {
+                freezePowerUp.interactable = false;
+            }
+            else if (coinsRemaining >= 750)
+            {
+                freezePowerUp.interactable = true;
+            }
+        }
         if (WaveSpawner.instance.wavesFinishedSpawning == true)
         {
             if (bossTimer > 0)
@@ -98,4 +135,41 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void SlowPowerUp()
+    {
+        Time.timeScale = 0.5f;
+        slowPowerUpActive = true;
+        slowPowerUp.interactable = false;
+        speedUp.interactable = false;
+        coinsRemaining -= 500;
+        Debug.Log("Slow Power-Up Started");
+        Invoke("NormalSpeed", 7.5f);
+    }
+
+    public void NormalSpeed()
+    {
+        Debug.Log("Slow Power-Up Ended");
+        Time.timeScale = 1;
+        slowPowerUpActive = false;
+        slowPowerUp.interactable = true;
+        speedUp.interactable = true;
+    }
+
+    public void FreezePowerUp()
+    {
+        currentEnemySpeed = Enemy.instance.movementSpeed;
+        Enemy.instance.movementSpeed = 0;
+        freezePowerUpActive = true;
+        coinsRemaining -= 750;
+        freezePowerUp.interactable = false;
+        Debug.Log("Freeze Power-Up Started");
+        Invoke("Unfreeze", 10);
+    }
+    public void Unfreeze()
+    {
+        Enemy.instance.movementSpeed = currentEnemySpeed;
+        freezePowerUpActive = false;
+        freezePowerUp.interactable = true;
+        Debug.Log("Freeze Power-Up Ended");
+    }
 }
